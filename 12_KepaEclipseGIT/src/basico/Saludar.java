3a7107a9da9abbb3aeadb6bce266691837107713
Curson mañana <Curson maana@PC15a5>
2018-01-29 10:56:28 +0100

    kihjij

--- 12_KepaEclipseGIT/src/basico/Saludar.java
+++ 12_KepaEclipseGIT/src/basico/Saludar.java
@@ -4,7 +4,7 @@ public class Saludar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola que tal     ?");
+		System.out.println("Hola que tal    jb ibni ?");
 		System.out.println("Bien y tú?");
 
 	}
